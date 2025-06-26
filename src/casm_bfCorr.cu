0365a4b78cbf709873ddf9585e9fe41eee1b7e88
liamconnor <liam.dean.connor@gmail.com>
2025-06-26 08:33:06 -0400

    Logger still.

--- src/casm_bfCorr.cu
+++ src/casm_bfCorr.cu
@@ -1487,7 +1487,7 @@ int main (int argc, char *argv[]) {
   
   syslog (LOG_INFO, "creating in and out hdus");
   
-  multi_log_t* log = multi_log_open("casm_bfCorr", LOG_INFO);
+  multilog_t* log = multi_log_open("casm_bfCorr", LOG_INFO);
   hdu_in  = dada_hdu_create (log);
   dada_hdu_set_key (hdu_in, in_key);
   if (dada_hdu_connect (hdu_in) < 0) {
@@ -1647,7 +1647,7 @@ int main (int argc, char *argv[]) {
   free(output_buffer);
   deallocate(&d,bf);
   dsaX_dbgpu_cleanup (hdu_in, hdu_out);
-  multi_log_close(log);
+  multilog_close(log);
 }
 
 
