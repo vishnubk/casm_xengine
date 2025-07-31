61f0915d0b4eaf976220bbe0e10aa673e5c132f4
liamconnor <liam.dean.connor@gmail.com>
2025-07-31 12:52:10 -0400

    fixing test.

--- tests/benchmark_bfCorr.py
+++ tests/benchmark_bfCorr.py
@@ -4,9 +4,16 @@ dir = '/home/user/software/casm_xengine/src'
 in_key = 'daaa'
 out_key = 'dddd'
 
+try:
+    os.system(f"dada_db -k {in_key} -d")
+    os.system(f"dada_db -k {out_key} -d")
+except:
+    pass
+
 os.system(f"dada_junkdb -k {in_key} -t 3600 header.txt")
 os.system(f"dada_db -k {in_key} -b 1073741824 -n 4")
 os.system(f"dada_db -k {out_key} -b 16777216 -n 4")
+time.sleep(1)
 os.system(f"{dir}/casm_bfCorr -b -i {in_key} \
           -o {out_key} -f {dir}/empty.flags \
           -a {dir}/dummy.calib -p {dir}/powers.out")
