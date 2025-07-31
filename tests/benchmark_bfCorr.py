912f1ed68f536009178a33a8da470406641978ce
liamconnor <liam.dean.connor@gmail.com>
2025-07-31 12:55:38 -0400

    adding fake writer

--- tests/benchmark_bfCorr.py
+++ tests/benchmark_bfCorr.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+junkdb = False
 dir = '/home/user/software/casm_xengine/src'
 in_key = 'daaa'
 out_key = 'dddd'
@@ -14,7 +15,11 @@ except:
 os.system(f"dada_db -k {in_key} -b 1073741824 -n 4")
 os.system(f"dada_db -k {out_key} -b 16777216 -n 4")
 time.sleep(1)
-os.system(f"dada_junkdb -k {in_key} -t 3600 header.txt")
+
+if junkdb:
+    os.system(f"dada_junkdb -k {in_key} -t 3600 header.txt")
+else:
+    os.system(f"{dir}/fake_writer ")
 time.sleep(1)
 os.system(f"{dir}/casm_bfCorr -b -i {in_key} \
           -o {out_key} -f {dir}/empty.flags \
